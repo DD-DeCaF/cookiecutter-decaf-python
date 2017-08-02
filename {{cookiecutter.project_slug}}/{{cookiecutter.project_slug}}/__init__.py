5ab9a03799c73d5e652036bb4ac0bc269dde7b39
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-08-02 13:53:07 +0200

    feat: establish absolute import for Python 2

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 __author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
